644fb4388e2f07972d49d7fed6a0cc8f77004964
Yashwanth vernekar <yashvernekar674@gmail.com>
2020-05-03 11:15:40 +0530

    Day 3: Intro to Conditional Statements
    Given an integer, N , perform the following conditional actions:
    
    If  N is odd, print Weird
    If  N is even and in the inclusive range of 2 to 5, print Not Weird
    If  N is even and in the inclusive range of 6 to 20, print Weird
    If  N is even and greater than 20 , print Not Weird
    Complete the stub code provided in your editor to print whether or not  is weird.

---
+++ Day3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(N%2!=0)
+    {
+        cout << "Weird" << endl;
+    }
+    else if((N%2==0) && (N>1) && (N<6))
+    {
+        cout << "Not Weird" << endl;
+    }
+    else if((N%2==0) && (N>5) && (N<21))
+    {
+        cout << "Weird" << endl;
+    }
+    else if((N%2==0) && (N>20))
+    {
+        cout << "Not Weird" << endl;
+    }
+    return 0;
+}
