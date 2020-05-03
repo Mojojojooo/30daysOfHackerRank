09185686f8d935624624ec3296635396bded1e66
Yashwanth vernekar <yashvernekar674@gmail.com>
2020-05-03 11:20:14 +0530

    Day 1: Data Types
    Complete the code in the editor below. The variables i,d ,s and  are already declared and initialized for you. You must:
    
    Declare  3 variables: one of type int, one of type double, and one of type String.
    Read  3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
    Use the + operator to perform the following operations:
    Print the sum of i plus your int variable on a new line.
    Print the sum of  d plus your double variable to a scale of one decimal place on a new line.
    Concatenate s with the string you read as input and print the result on a new line.
    Note: If you are using a language that doesn't support using + for string concatenation (e.g.: C), you can just print one variable immediately following the other on the same line. The string provided in your editor must be printed first, immediately followed by the string you read as input.

--- Day1.cpp
+++ Day1.cpp
@@ -3,7 +3,7 @@
 #include <limits>
 
 using namespace std;
-
+{
 int main() {
     int i = 4;
     double d = 4.0;
@@ -18,17 +18,6 @@ int main() {
     getline(cin,s1);
 
     cout << a+i << endl << fixed << setprecision(1) << b+d << endl << s+s1;
-    
-    // Declare second integer, double, and String variables.
-    
-    // Read and save an integer, double, and String to your variables.
-    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
-    
-    // Print the sum of both integer variables on a new line.
-    
-    // Print the sum of the double variables on a new line.
-    
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
 
     return 0;
+}
